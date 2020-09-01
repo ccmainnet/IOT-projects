84f633a0caed885b4235815d9f0c96fad484aacf
Bob Nieuwenhuis <bobnieuwenhuis@gmail.com>
2020-09-01 13:02:23 +0200

    Initial Commit

---
+++ headsortails_functions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  headsortails_functions
+//
+//  Created by Bob on 01/09/2020.
+//  Copyright © 2020 CCMainnet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
